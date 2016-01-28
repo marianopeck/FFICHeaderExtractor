8a27e924666d7f8a33654bc0e5fa395c18e27dd6
Mariano Martinez Peck <marianopeck@gmail.com>
2016-01-28 16:37:36 -0300

    trying my improvements to Test runner

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -6,17 +6,11 @@ wget --quiet -O - get.pharo.org/alpha+vm | bash
 
 ./pharo Pharo.image eval --save "
 
-Author fullName: 'Travis'.
-
 Metacello new 
 	baseline: 'FFICHeaderExtractor';
 	repository: 'filetree://repository';
 	load.
 	
-	Gofer it
-		url: 'http://smalltalkhub.com/mc/marianopeck/MarianoPublic/main';
-		package: 'SUnit-UI';
-	load.	
 "
 
 ./pharo Pharo.image test --no-xterm --fail-on-failure "FFICHeaderExtractor.*" 2>&1
